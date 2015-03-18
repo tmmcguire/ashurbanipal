884662202ae1cd038ef0fca3adc515eedc6b7821
Tommy M. McGuire <mcguire@crsr.net>
2015-03-18 09:39:09 -0500

    clean up json conversion and make extjs happy

--- src/net/crsr/ashurbanipal/StoreToJson.java
+++ src/net/crsr/ashurbanipal/StoreToJson.java
@@ -18,7 +18,7 @@ import net.crsr.ashurbanipal.store.MetadataStore;
 import net.crsr.ashurbanipal.store.PosStore;
 import net.crsr.ashurbanipal.store.WordStore;
 
-import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class StoreToJson {
 
@@ -33,80 +33,49 @@ public class StoreToJson {
       posStore.read();
       WordStore nounStore = new WordStore(args[3]);
       nounStore.read();
-      
+
       final Map<String,Integer> filenameToEtextNo = formatStore.asEtextNoLookupMap();
-      final Map<String,Integer> filenameToFileId = fileIdMapping(filenameToEtextNo);
-      
       final Map<String,Set<String>> nouns = nounStore.asSetsOfWords();
 
-      writeFileEtextMap(etextLookup(filenameToFileId, filenameToEtextNo));
-      writeMetadata(metadataArray(metadataStore));
-      writePOS(posVectors(filenameToFileId, posStore));
-      writeNouns(wordSetsForFiles(filenameToFileId, nouns));
+      writeMetadata(metadataArray(metadataStore, formatStore));
+      writePOS(posVectors(filenameToEtextNo, posStore));
+      writeNouns(wordSetsForFiles(filenameToEtextNo, nouns));
 
     } catch (IOException e) {
       throw new IOError(e);
     }
   }
 
-  private static Map<String,Integer> fileIdMapping(Map<String,?> map) {
-    int count = 0;
-    final Map<String,Integer> result = new HashMap<>();
-    for (String filename : map.keySet()) {
-      result.put(filename, count++);
-    }
-    return result;
-  }
-
-  private static List<Integer> etextLookup(Map<String,Integer> fileIds, Map<String,Integer> etextLookup) {
-    List<Integer> array = new ArrayList<>(etextLookup.size());
-    for (int i = 0; i < etextLookup.size(); ++i) { array.add(null); }
-    for (Entry<String,Integer> entry : etextLookup.entrySet()) {
-      final Integer fileId = fileIds.get(entry.getKey());
-      final Integer etextNo = entry.getValue();
-      array.set(fileId, etextNo);
-    }
-    return array;
-  }
-  
-  private static Integer maxOfSet(Set<Integer> ints) {
-    Integer max = null;
-    for (Integer i : ints) {
-      if (max == null || max < i) { max = i; }
-    }
-    return max;
-  }
-
-  private static List<Map<String,List<String>>> metadataArray(MetadataStore metadata) {
-    int nRows = maxOfSet(metadata.keySet()) + 1;
-    final List<Map<String,List<String>>> array = new ArrayList<>(nRows);
-    for (int i = 0; i < nRows; ++i) { array.add(null); }
+  private static List<Map<String,List<String>>> metadataArray(MetadataStore metadata, Map<Integer,?> formatStore) {
+    final List<Map<String,List<String>>> array = new ArrayList<>();
     for (Entry<Integer,Map<String,List<String>>> entry : metadata.entrySet()) {
       final Integer etextNo = entry.getKey();
       final Map<String,List<String>> data = entry.getValue();
-      array.set(etextNo, data);
+      if (formatStore.containsKey(etextNo)) {
+        array.add(data);
+      }
     }
     return array;
   }
-  
+
   private static List<Double> dataVector(List<String> columns, Map<String,Double> metadata) {
     final List<Double> vector = new ArrayList<>();
     for (String column : columns.subList(1, columns.size())) { vector.add(metadata.get(column)); }
     return vector;
   }
-  
-  private static List<List<Double>> posVectors(Map<String,Integer> fileIds, PosStore posStore) {
+
+  private static List<Map<String,Object>> posVectors(Map<String,Integer> fileToEtext, PosStore posStore) {
     final List<String> columns = posStore.columns();
-    final List<List<Double>> vectors = new ArrayList<>(fileIds.size());
-    for (int i = 0; i < fileIds.size(); ++i) { vectors.add(null); }
+    final List<Map<String,Object>> vectors = new ArrayList<>();
     for (Entry<String,Map<String,Double>> entry : posStore.entrySet()) {
-      int fileId = fileIds.get(entry.getKey());
-      List<Double> vector = dataVector(columns, entry.getValue());
-      vectors.set(fileId, vector);
+      final Map<String,Object> row = new HashMap<>();
+      row.put("vector", dataVector(columns, entry.getValue()));
+      row.put("etext_no", fileToEtext.get(entry.getKey()));
+      vectors.add(row);
     }
     return vectors;
   }
-  
+
   private static Map<String,Integer> wordToId(Map<?,Set<String>> wordsByFile) {
     final Set<String> words = new HashSet<>();
     for (Set<String> fromFile : wordsByFile.values()) {
@@ -119,7 +88,7 @@ public class StoreToJson {
     }
     return result;
   }
-  
+
   private static List<Integer> fileToBitset(Map<String,Integer> wordId, Set<String> words) {
     int nBits = wordId.size();
     int nBitmaps = nBits / 32 + 1;
@@ -131,43 +100,28 @@ public class StoreToJson {
     }
     return bitset;
   }
-  
-  private static List<List<Integer>> wordSetsForFiles(Map<String,Integer> fileIds, Map<String,Set<String>> words) {
+
+  private static List<Map<String,Object>> wordSetsForFiles(Map<String,Integer> fileToEtext, Map<String,Set<String>> words) {
     final Map<String,Integer> wordIds = wordToId(words);
-    final List<List<Integer>> wordsets = new ArrayList<>(fileIds.size());
-    for (int i = 0; i < fileIds.size(); ++i) { wordsets.add(null); }
+    final List<Map<String,Object>> wordsets = new ArrayList<>();
     for (Entry<String,Set<String>> entry : words.entrySet()) {
-      final int fileId = fileIds.get(entry.getKey());
-      final List<Integer> wordset = fileToBitset(wordIds, entry.getValue());
-      wordsets.set(fileId, wordset);
+      final Map<String,Object> row = new HashMap<>();
+      row.put("etext_no", fileToEtext.get(entry.getKey()));
+      row.put("wordset", fileToBitset(wordIds, entry.getValue()));
+      wordsets.add(row);
     }
     return wordsets;
   }
 
-  private static void writeFileEtextMap(List<Integer> idToEtext) throws IOException {
-    final File newFile = new File("FileToEtext.js");
-    OutputStream w = null;
-    try {
-      w = new FileOutputStream(newFile);
-      w.write("// This array maps file identifiers to etext-numbers.\n".getBytes());
-      w.write("fileToEtext = ".getBytes());
-      w.write(JSONArray.toJSONString(idToEtext).getBytes());
-      w.write("\n".getBytes());
-    } finally {
-      if (w != null) {
-        w.close();
-      }
-    }
-  }
-  
   private static void writeMetadata(List<Map<String,List<String>>> metadata) throws IOException {
-    final File newFile = new File("Metadata.js");
+    // put the result in the format Ext wants
+    final Map<String,Object> object = new HashMap<>();
+    object.put("rows", metadata);
+    final File newFile = new File("metadata.json");
     OutputStream w = null;
     try {
       w = new FileOutputStream(newFile);
-      w.write("// This array maps etext numbers to etext metadata.\n".getBytes());
-      w.write("metadata = ".getBytes());
-      w.write(JSONArray.toJSONString(metadata).getBytes());
+      w.write(JSONObject.toJSONString(object).getBytes());
       w.write("\n".getBytes());
     } finally {
       if (w != null) {
@@ -175,15 +129,15 @@ public class StoreToJson {
       }
     }
   }
-  
-  private static void writePOS(List<List<Double>> pos) throws IOException {
-    final File newFile = new File("StyleData.js");
+
+  private static void writePOS(List<Map<String,Object>> rows) throws IOException {
+    final Map<String,Object> object = new HashMap<>();
+    object.put("rows", rows);
+    final File newFile = new File("styledata.json");
     OutputStream w = null;
     try {
       w = new FileOutputStream(newFile);
-      w.write("// This array maps file ids to etext style data vectors.\n".getBytes());
-      w.write("styleData = ".getBytes());
-      w.write(JSONArray.toJSONString(pos).getBytes());
+      w.write(JSONObject.toJSONString(object).getBytes());
       w.write("\n".getBytes());
     } finally {
       if (w != null) {
@@ -191,15 +145,15 @@ public class StoreToJson {
       }
     }
   }
-  
-  private static void writeNouns(List<List<Integer>> bitsets) throws IOException {
-    final File newFile = new File("TopicData.js");
+
+  private static void writeNouns(List<Map<String,Object>> wordsets) throws IOException {
+    final Map<String,Object> object = new HashMap<>();
+    object.put("rows", wordsets);
+    final File newFile = new File("topicdata.json");
     OutputStream w = null;
     try {
       w = new FileOutputStream(newFile);
-      w.write("// This array maps file ids to sets of topic words.\n".getBytes());
-      w.write("topicData = ".getBytes());
-      w.write(JSONArray.toJSONString(bitsets).getBytes());
+      w.write(JSONObject.toJSONString(object).getBytes());
       w.write("\n".getBytes());
     } finally {
       if (w != null) {
