0d0716d52ccb113e3380ccef998e5b82ee6ceb79
Tommy M. McGuire <mcguire@crsr.net>
2015-09-14 14:05:39 -0500

    update wordstore-to-bitset for etext_no keys

--- src/net/crsr/ashurbanipal/WordStoreToBitSet.java
+++ src/net/crsr/ashurbanipal/WordStoreToBitSet.java
@@ -5,34 +5,32 @@ import java.io.FileWriter;
 import java.io.IOError;
 import java.io.IOException;
 import java.io.Writer;
-import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 
-import net.crsr.ashurbanipal.store.FormatStore;
 import net.crsr.ashurbanipal.store.WordStore;
 
-public class TopicToBitSet {
+public class WordStoreToBitSet {
 
   public static void main(String[] args) {
     try {
-      
+
       final WordStore wordStore = new WordStore(args[0]);
       wordStore.read();
-      
-      final FormatStore formatStore = new FormatStore(args[1]);
-      formatStore.read();
-      final Map<String,Integer> fileToEtext = formatStore.asEtextNoLookupMap();
 
+      // Read all of the words in the store, counting the
+      // number of files using them.
       final Map<String,Integer> words = new TreeMap<>();
       for (Map<String,Integer> map : wordStore.values()) {
         for (String word : map.keySet()) {
           words.put(word, words.getOrDefault(word, 0) + 1);
         }
       }
-      
+
+      // Create a mapping of words (used by more than one file
+      // and all alphabetic) to identifying numbers.
       final Map<String,Integer> wordIds = new HashMap<>();
       int i = 0;
       for (Entry<String,Integer> word : words.entrySet()) {
@@ -42,18 +40,15 @@ public class TopicToBitSet {
         }
       }
       System.out.println(wordIds.size() + " words");
-      
-      Writer writer = new BufferedWriter(new FileWriter(args[2]));
-      for (String file : wordStore.keySet()) {
-        final Integer etext_no = fileToEtext.get(file);
+
+      // For each text, write the etext_no and the id of each word to a line.
+      final Writer writer = new BufferedWriter(new FileWriter(args[1]));
+      for (Integer etext_no : wordStore.keySet()) {
         final StringBuilder sb = new StringBuilder().append(etext_no);
-//        System.out.println(etext_no);
-//        BigInteger bitSet = BigInteger.valueOf(0);
-        for (String word : wordStore.get(file).keySet()) {
+        for (String word : wordStore.get(etext_no).keySet()) {
           final Integer bit = wordIds.get(word);
           if (bit != null) {
             sb.append('\t').append(bit);
-//            bitSet = bitSet.setBit( bit );
           }
         }
         sb.append('\n');
@@ -66,7 +61,7 @@ public class TopicToBitSet {
     }
 
   }
-  
+
   private static boolean isAlpha(String s) {
     for (char ch : s.toCharArray()) {
       if (!Character.isLetter(ch)) {
