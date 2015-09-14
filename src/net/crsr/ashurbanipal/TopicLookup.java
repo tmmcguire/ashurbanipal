38987d8a0c6f936e3288c8bbf07c29534d32e245
Tommy M. McGuire <mcguire@crsr.net>
2015-09-14 13:03:48 -0500

    update topic-lookup for etext_no keys

--- src/net/crsr/ashurbanipal/TopicLookup.java
+++ src/net/crsr/ashurbanipal/TopicLookup.java
@@ -1,5 +1,8 @@
 package net.crsr.ashurbanipal;
 
+import static net.crsr.ashurbanipal.utility.LookupUtilites.printHeader;
+import static net.crsr.ashurbanipal.utility.LookupUtilites.printMetadata;
+
 import java.io.IOError;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,71 +13,47 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
-import net.crsr.ashurbanipal.store.FormatStore;
 import net.crsr.ashurbanipal.store.MetadataStore;
 import net.crsr.ashurbanipal.store.WordStore;
 import net.crsr.ashurbanipal.utility.Pair;
-
-import static net.crsr.ashurbanipal.LookupUtilites.*;
 
 public class TopicLookup {
 
   public static void main(String[] args) {
     try {
 
-      final int etextNo = Integer.parseInt(args[3]);
+      final int etextNo = Integer.parseInt(args[2]);
 
-      final FormatStore formatStore = new FormatStore(args[0]);
-      formatStore.read();
-      final List<Pair<String,String>> formats = formatStore.get(etextNo);
-      final Map<String,Integer> etextLookupMap = formatStore.asEtextNoLookupMap();
-
-      final MetadataStore metadataStore = new MetadataStore(args[1]);
+      final MetadataStore metadataStore = new MetadataStore(args[0]);
       metadataStore.read();
-      final Map<String,List<String>> metadata = metadataStore.get(etextNo);
-
-      final WordStore nounStore = new WordStore(args[2]);
+      final WordStore nounStore = new WordStore(args[1]);
       nounStore.read();
 
-      final List<Map<String,Integer>> matches = matchingNouns(nounStore, formats);
-      for (Map<String,Integer> match : matches) {
-        printHeader();
-        printMetadata(metadata);
-        for (Pair<Double,String> neighbors : nearestNeighbors(nounStore, match)) {
-          int neighborNo = etextLookupMap.get(neighbors.r);
-          if (neighborNo != etextNo) {
-            printMetadata(neighbors.l, metadataStore.get(neighborNo));
-          }
+      printHeader();
+      printMetadata(metadataStore.get(etextNo));
+      for (Pair<Double,Integer> neighbors : nearestNeighbors(nounStore, nounStore.get(etextNo))) {
+        if (neighbors.r != etextNo) {
+          printMetadata(neighbors.l, metadataStore.get(neighbors.r));
         }
         System.out.println();
       }
 
     } catch (ArrayIndexOutOfBoundsException e) {
-      System.out.println("Usage: TopicLookup format-store metadata-store noun-store etext-no");
+      System.out.println("Usage: TopicLookup metadata-store noun-store etext-no");
     } catch (IOException e) {
       throw new IOError(e);
     }
   }
 
-  public static List<Map<String,Integer>> matchingNouns(WordStore nounStore, List<Pair<String,String>> formats) {
-    final List<Map<String,Integer>> result = new ArrayList<>();
-    for (Pair<String,String> format : formats) {
-      if (format.l.contains("text/plain") && nounStore.containsKey(format.r)) {
-        result.add(nounStore.get(format.r));
-      }
-    }
-    return result;
-  }
+  public static List<Pair<Double,Integer>> nearestNeighbors(WordStore posStore, Map<String,Integer> initial) {
+    final List<Pair<Double,Integer>> results = new ArrayList<>();
 
-  public static List<Pair<Double,String>> nearestNeighbors(WordStore posStore, Map<String,Integer> initial) {
-    final List<Pair<Double,String>> results = new ArrayList<>();
-
-    for (Entry<String,Map<String,Integer>> entry : posStore.entrySet()) {
+    for (Entry<Integer,Map<String,Integer>> entry : posStore.entrySet()) {
       results.add(Pair.pair(computeDistance(initial, entry.getValue()), entry.getKey()));
     }
-    results.sort(new Comparator<Pair<Double,String>>() {
+    results.sort(new Comparator<Pair<Double,Integer>>() {
       @Override
-      public int compare(Pair<Double,String> left, Pair<Double,String> right) {
+      public int compare(Pair<Double,Integer> left, Pair<Double,Integer> right) {
         return Double.compare(left.l, right.l);
       }
     });
